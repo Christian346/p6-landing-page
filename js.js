133dc9e18742c2eacb5d1e16a2c5d1abe4e0676d
Christian <chris.346@hotmail.com>
2021-11-23 16:30:11 -0500

    project version-1

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ var menubtn = document.querySelector('.menu-btn')
 var triangle = document.querySelector('.triangle')
 var menuphone = document.querySelector('.main-nav-li')
 
-
+//recheck addeventListeners and how they work!
 menubtn.addEventListener('click', show)
 
 function show(){  
